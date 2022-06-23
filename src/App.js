2db68ffbd554a3f1cd77c2d10bed734a086c8576
cedrus172 <cedrusisme@gmail.com>
2022-06-23 23:31:41 +0700

    first update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  const listStudent = [
+    {id : 1, name : 'Trieu Huy Hoang', age : 22, address : 'Nam Dinh'},
+    {id : 2, name : 'Trieu Huy Hoang 2', age : 22, address : 'Ha Noi'}
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Table listStudent={listStudent}/>
+    </>
   );
+}
+
+const Table = ({listStudent}) =>{
+  return (<div>
+    <table>
+      <thead>
+        <tr>
+          <td>
+            Id
+          </td>
+          <td>
+            Name
+          </td>
+          <td>Age</td>
+          <td>
+            Address
+          </td>
+        </tr>
+      </thead>
+      <tbody>
+{
+  listStudent.map((student)=>{
+    return <Row id={student.id} name={student.name} age={student.age} address={student.address} />
+  })
+}
+      </tbody>
+    </table>
+  </div>)
+}
+
+const Row = ({id, name, age, address})=>{
+  return (<tr>
+    <td>
+      {id}
+    </td>
+    <td>
+      {name}
+    </td>
+    <td>
+      {age}
+    </td>
+    <td>
+      {address}
+    </td>
+  </tr>)
 }
 
 export default App;
